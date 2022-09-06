13a345f36108a9f62847e4e25fc47a3d3475a962
Edward Ijeruh <zacharaiah17@gmail.com>
2022-09-06 14:21:03 +0100

    Resident home page and unique OTP page

---
+++ js/unique-code.js
@@ -0,0 +1,9 @@
+let otpSuccessMessage = document.querySelector('.otp__success-message');
+let verifyBtn = document.getElementById('verify__btn');
+
+verifyBtn.addEventListener('click', () => {
+    otpSuccessMessage.classList.toggle('active')
+    setTimeout(() => {
+        otpSuccessMessage.classList.add('d-none');
+    }, 2000);
+})
